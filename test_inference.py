7c16702174f46d1b238bd0e3fc718506257bc831
Christopher Park <parkchris252@gmail.com>
2025-12-26 15:20:52 -0800

    fixed model output truncating

--- test_inference.py
+++ test_inference.py
@@ -1,5 +1,4 @@
 import torch
-import os
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from check_cache import check_model_cache
 from dotenv import load_dotenv
@@ -33,14 +32,16 @@ inputs = {k: v.to(device) for k, v in inputs.items()}
 
 outputs = model.generate(
     **inputs,
-    max_new_tokens=512,
+    max_new_tokens=100,
     temperature=0.7,
     do_sample=True,
-    pad_token_id=tokenizer.eos_token_id,
-    eos_token_id=tokenizer.eos_token_id
+    pad_token_id=tokenizer.eos_token_id
 )
 
 generated_tokens = outputs[0][inputs['input_ids'].shape[1]:]
 answer = tokenizer.decode(generated_tokens, skip_special_tokens=True)
-print(answer)
 
+# Stop at the first new question
+answer = answer.split("\nQuestion:")[0].strip()
+
+print(answer)
